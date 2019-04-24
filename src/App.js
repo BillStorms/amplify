b55c1de42636f32df7046a7d6b09aeec85d2a9c7
Bill Storms <bstorms@cafepress.com>
2019-04-23 22:18:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bill Is Learning React! 
         </a>
       </header>
     </div>
